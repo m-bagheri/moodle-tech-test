4f625a8367a01c899c13877c4bc2443fff79819c
m-bagheri <m.bagheri1987@gmail.com>
2023-02-08 10:42:45 +1030

    Fixing bugs and typos Improving code to reduce number of sql queries to speed up the process as well as reduce amount of resources required

--- DeleteUsers.php
+++ DeleteUsers.php
@@ -1,49 +1,58 @@
 <?php
 
 /**
-* Remove all users (or one user) from all groups in course
-* @param int $courseid
-* @param int $suerid 0 means all users
-* @param bool $hidefeedback
-* @return bool success
-*/
-function groups_delete_group_memebers($courseid, $userid=0, $showfeedback=false) {
-	global $DB, $OUTPUT;
-	$allusers = false;
+ * Remove all users (or one user) from all groups in course
+ * @param int $courseid
+ * @param int $suerid 0 means all users
+ * @param bool $showfeedback
+ * @return bool success
+ */
+function groups_delete_group_memebers(int $courseid, int $userid = 0, bool $showfeedback = false)
+{
+    global $DB, $OUTPUT;
 
-	$groups = $DB->get_recordset('groups', array('courseid' => $courseid));
-	foreach ($groups as $group) {
-    	    if ($userid) {
-        	$userids = array($userid);
-    	    } else {
-        	$params = array('groupid' => $group->id);
-        	$userid = $DB->get_fieldset_select('groups_members',
-                	'userid',
-                	'groupid = :groupid',
-                	$params);
-    	    }
-    	    foreach ($userids as $id) {
-        	groups_remove_member($group, $id);
-    	    }
-	}
-	return true;
+    $results = [];
+
+    $groups = $DB->get_recordset('groups', array('courseid' => $courseid));
+    foreach ($groups as $group) {
+        $results[] = groups_remove_member($group->id, $userid);
+    }
+
+
+    // checking to see if we had failure in removing members from any groups in the course
+    $failure = in_array(false, $results);
+
+    if($showfeedback) {
+        $OUTPUT = [
+          'status' => $failure ? 'failed' : 'successful',
+          'message' => $failure ? 'Some members failed to be removed from course' : 'Members successfully removed from course',
+        ];
+    }
+
+    // if we had any failure then it would return false
+    return !$failure;
 }
 
 /**
  * Deletes the link between the specified user and group.
  *
- * @param  	mixed $groupid  The groupid
- * @param  	mixed $userid   The userid
+ * @param int $groupid The groupid
+ * @param int $userid 0 means all users
  * @return bool True if deletion was successful, false otherwise
  */
-function groups_remove_member($groupid, $userid) {
-	global $DB;
+function groups_remove_member(int $groupid, int $userid = 0)
+{
+    global $DB;
 
-	$params = array('groupid' => $groupid, 'userid' => $userid);
-	if (!$DB->record_exists('group_members', $params)) {
-    	    return true;
-	}
+    $params = ['groupid' => $groupid];
+    if($userid != 0) {
+        $params = array_merge($params, ['userid' => $userid]);
+    }
+    try {
+        $DB->delete_records('groups_members', $params);
+    } catch (Exception $e) {
+        return false;
+    }
 
-	$DB->delete_records('groups_members', $params);
-	return true;
+    return true;
 }
